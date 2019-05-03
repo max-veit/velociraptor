e8e9098b96c7159cbad946ecc7825dc69a7a6c6e
dilkins <wilkinsd@outlook.com>
2019-05-03 10:54:31 +0200

    typo fixed

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 if [ ! -d lib ];then mkdir lib;fi
 if [ ! -d lib/SOAPFAST ];then cd lib;git clone https://github.com/dilkins/SOAPFAST.git;cd ../;fi
-cd lib/SOAPFAST/src;make;cd ../../../
+cd lib/SOAPFAST/soapfast;make;cd ../../../
