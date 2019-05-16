c3c600d00b1a0c5c4e65d2635c4b6d8a1bd8cbcd
dilkins <wilkinsd@outlook.com>
2019-05-16 17:11:29 +0200

    More changes made to script

--- get_prediction_kernels.sh
+++ get_prediction_kernels.sh
@@ -11,24 +11,24 @@ for arg in $(seq 1 $#);do
  fi
 done
 
-mkdir prediction_files
+mkdir prediction_files_${suffix}
 
 # Get L=0 power spectrum
-get_power_spectrum.py -rc ${rcut} -c H C N O S Cl -s H C N O S Cl -lm 0 -sf PS_files/PS0_train -f ${fname} -o prediction_files/PS0_pred
+get_power_spectrum.py -rc ${rcut} -c H C N O S Cl -s H C N O S Cl -lm 0 -sf PS_files/PS0_train -f ${fname} -o prediction_files_${suffix}/PS0_pred
 
 # Get L=1 power spectrum
-get_power_spectrum.py -rc ${rcut} -c H C N O S Cl -s H C N O S Cl -lm 1 -sf PS_files/PS1_train -f ${fname} -o prediction_files/PS1_pred
+get_power_spectrum.py -rc ${rcut} -c H C N O S Cl -s H C N O S Cl -lm 1 -sf PS_files/PS1_train -f ${fname} -o prediction_files_${suffix}/PS1_pred
 
 # Get atomic power spectra
-get_atomic_power_spectrum.py -lm 0 -p prediction_files/PS0_pred.npy -o prediction_files/PS0_pred_atomic -f ${fname}
-get_atomic_power_spectrum.py -lm 1 -p prediction_files/PS1_pred.npy -o prediction_files/PS1_pred_atomic -f ${fname}
+get_atomic_power_spectrum.py -lm 0 -p prediction_files_${suffix}/PS0_pred.npy -o prediction_files_${suffix}/PS0_pred_atomic -f ${fname}
+get_atomic_power_spectrum.py -lm 1 -p prediction_files_${suffix}/PS1_pred.npy -o prediction_files_${suffix}/PS1_pred_atomic -f ${fname}
 
 # Get prediction kernels
 # L=0 kernel
-get_kernel.py -lm 0 -z 2 -ps PS_files/PS0_train_atomic_sparse.npy prediction_files/PS0_pred_atomic.npy -ps0 PS_files/PS0_train_atomic_sparse.npy prediction_files/PS0_pred_atomic.npy -s NONE NONE -o K0_${suffix}
+get_kernel.py -lm 0 -z 2 -ps PS_files/PS0_train_atomic_sparse.npy prediction_files_${suffix}/PS0_pred_atomic.npy -ps0 PS_files/PS0_train_atomic_sparse.npy prediction_files_${suffix}/PS0_pred_atomic.npy -s NONE NONE -o K0_${suffix}
 
 # L=1 kernel
-get_kernel.py -lm 1 -z 2 -ps PS_files/PS1_train_atomic_sparse.npy prediction_files/PS1_pred_atomic.npy -ps0 PS_files/PS0_train_atomic_sparse.npy prediction_files/PS0_pred_atomic.npy -s NONE NONE -o K1_${suffix}
+get_kernel.py -lm 1 -z 2 -ps PS_files/PS1_train_atomic_sparse.npy prediction_files_${suffix}/PS1_pred_atomic.npy -ps0 PS_files/PS0_train_atomic_sparse.npy prediction_files_${suffix}/PS0_pred_atomic.npy -s NONE NONE -o K1_${suffix}
 
 # Convert spherical kernel to vector kernel
-spherical_to_cartesian_kernel.py -k K1_TT.npy -o Kvec_TT.npy
+spherical_to_cartesian_kernel.py -k K1_${suffix}.npy -o Kvec_${suffix}.npy
