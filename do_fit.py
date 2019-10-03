Fix even stupider numpy broadcasting problem

do_fit.py
@@ -156,7 +156,7 @@ if __name__ == "__main__":
     del args.dipoles
     natoms_list = [geom.get_number_of_atoms() for geom in geometries]
     if args.dipole_normalize:
-        dipoles = dipoles / natoms_list[:,np.newaxis]
+        dipoles = (dipoles.T / natoms_list).T
     n_descriptors = sum(natoms_list)
     (scalar_kernel_sparse, scalar_kernel_full_sparse,
      tensor_kernel_sparse, tensor_kernel_full_sparse) = load_kernels(args)

get_residuals.py
@@ -106,7 +106,7 @@ if __name__ == "__main__":
     charges = get_charges(geometries)
     dipoles = np.loadtxt(args.dipoles)
     if args.dipole_normalized:
-        dipoles = dipoles / natoms_list[:,np.newaxis]
+        dipoles = (dipoles.T / natoms_list).T
     weights = np.load(args.weights)
     del args.dipoles
     del args.weights