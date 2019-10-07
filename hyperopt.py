2448572317539dd72c0c7b34922635f569470aab
Max Veit <maxdveit@gmail.com>
2019-10-07 16:05:51 +0200

    Clean up hyperopt script and make a bit more modular

--- hyperopt.py
+++ hyperopt.py
@@ -14,24 +14,26 @@ from velociraptor.fitutils import (transform_kernels, transform_sparse_kernels,
                                    get_charges)
 
 PY2_EXEC = "/home/veit/miniconda3/envs/py2-compat/bin/python2"
-SOAPFAST_PATH = "/home/veit/SOAPFAST"
+SOAPFAST_PATH = "/home/veit/SOAPFAST/soapfast"
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
 
 
-def compute_power_spectra(n_max, l_max, atom_width, rad_r0, rad_m,
-                          ps_prefix='PS0', atoms_file='qm7.xyz', workdir=None,
-                          n_sparse_envs=2000, feat_sparsefile=None):
+def compute_scalar_power_spectra(
+        n_max, l_max, atom_width, rad_r0, rad_m, ps_prefix='PS0',
+        atoms_file='qm7.xyz', workdir=None, n_sparse_envs=2000,
+        n_sparse_components=500, feat_sparsefile=None):
     # compute powerspectra (soapfast)
+    PY2_EXEC = os.environ.get('PY2_EXEC', 'python')
+    SOAPFAST_PATH = os.environ.get('SOAPFAST_PATH', '')
     # These PS parameters are unlikely to change
     rad_c = 1
     r_cut = 5.0
-    n_sparse_components = 500
     if not os.path.dirname(atoms_file):
         atoms_file = os.path.join(workdir, atoms_file)
     ps_args = ([
         PY2_EXEC,
-        os.path.join(SOAPFAST_PATH, 'soapfast', 'get_power_spectrum.py'),
+        os.path.join(SOAPFAST_PATH, 'get_power_spectrum.py'),
         '-f', atoms_file,
         '-n', str(n_max), '-l', str(l_max), '-rc', str(r_cut),
         '-sg', str(atom_width),
@@ -48,7 +50,7 @@ def compute_power_spectra(n_max, l_max, atom_width, rad_r0, rad_m,
     # ATOMIC POWER spectra!
     atomic_ps_args = [
         PY2_EXEC,
-        os.path.join(SOAPFAST_PATH, 'soapfast', 'scripts',
+        os.path.join(SOAPFAST_PATH, 'scripts',
                      'get_atomic_power_spectrum.py'),
         '-p', os.path.join(workdir, ps_prefix + '.npy'),
         '-f', atoms_file,
@@ -61,7 +63,7 @@ def compute_power_spectra(n_max, l_max, atom_width, rad_r0, rad_m,
     if (n_sparse_envs is not None) and (n_sparse_envs > 0):
         fps_args = [
             PY2_EXEC,
-            os.path.join(SOAPFAST_PATH, 'soapfast', 'scripts', 'do_fps.py'),
+            os.path.join(SOAPFAST_PATH, 'scripts', 'do_fps.py'),
             '-p', os.path.join(workdir, ps_prefix + '_atomic.npy'),
             #'-s', os.path.join(workdir, ps_prefix + '_natoms.npy'),
             '-n', str(n_sparse_envs),
@@ -72,8 +74,10 @@ def compute_power_spectra(n_max, l_max, atom_width, rad_r0, rad_m,
         subprocess.run(fps_args)
 
 
-def compute_kernel(ps_name, ps_second_name=None, kernel_name='K0_MM',
-                   zeta=2, workdir=None):
+def compute_scalar_kernel(ps_name, ps_second_name=None, kernel_name='K0_MM',
+                          zeta=2, workdir=None):
+    PY2_EXEC = os.environ.get('PY2_EXEC', 'python')
+    SOAPFAST_PATH = os.environ.get('SOAPFAST_PATH', '')
     if not os.path.dirname(ps_name):
         ps_name = os.path.join(workdir, ps_name)
     if (ps_second_name is not None) and not os.path.dirname(ps_second_name):
@@ -85,7 +89,7 @@ def compute_kernel(ps_name, ps_second_name=None, kernel_name='K0_MM',
         ps_files = [ps_name, ]
     kernel_args = ([
         PY2_EXEC,
-        os.path.join(SOAPFAST_PATH, 'soapfast', 'get_kernel.py'),
+        os.path.join(SOAPFAST_PATH, 'get_kernel.py'),
         '-z', str(zeta),
         '-ps', ] + ps_files + ['-ps0',] + ps_files + [
         '-o', os.path.join(workdir, kernel_name)
@@ -94,32 +98,42 @@ def compute_kernel(ps_name, ps_second_name=None, kernel_name='K0_MM',
     subprocess.run(kernel_args)
 
 
-def compute_scalar_residual(n_max, l_max, atom_width, rad_r0, rad_m,
-                            dipole_reg, charge_reg, n_sparse_envs=2000,
-                            workdir=None, print_progress=True,
-                            dipole_normalize=True):
+def recompute_scalar_kernels(n_max, l_max, atom_width, rad_r0, rad_m,
+                             n_sparse_envs=2000, n_sparse_components=500,
+                             workdir=None):
     # number of sparse envs is another convergence parameter
-    compute_power_spectra(n_max, l_max, atom_width, rad_r0, rad_m,
-                          ps_prefix='PS0_train', atoms_file='qm7_train.xyz',
-                          workdir=workdir, n_sparse_envs=n_sparse_envs)
+    compute_scalar_power_spectra(
+            n_max, l_max, atom_width, rad_r0, rad_m, ps_prefix='PS0_train',
+            atoms_file='qm7_train.xyz', workdir=workdir,
+            n_sparse_envs=n_sparse_envs,
+            n_sparse_components=n_sparse_components)
     # Make sure feat sparsification uses the PS0_train values!
     # (and don't sparsify on envs)
-    compute_power_spectra(n_max, l_max, atom_width, rad_r0, rad_m,
-                          ps_prefix='PS0_test', atoms_file='qm7_test.xyz',
-                          workdir=workdir, feat_sparsefile='PS0_train',
-                          n_sparse_envs=-1)
+    compute_scalar_power_spectra(
+            n_max, l_max, atom_width, rad_r0, rad_m, ps_prefix='PS0_test',
+            atoms_file='qm7_test.xyz', workdir=workdir,
+            feat_sparsefile='PS0_train', n_sparse_envs=-1)
     # compute kernels (soapfast)
     zeta = 2 # possibly another parameter to gridsearch
     # sparse-sparse
-    compute_kernel('PS0_train_atomic_sparse.npy', kernel_name='K0_MM',
-                   zeta=zeta, workdir=workdir)
+    compute_scalar_kernel('PS0_train_atomic_sparse.npy', kernel_name='K0_MM',
+                          zeta=zeta, workdir=workdir)
     # full-sparse
-    compute_kernel('PS0_train_atomic.npy', 'PS0_train_atomic_sparse.npy',
-                   zeta=zeta, kernel_name='K0_NM', workdir=workdir)
+    compute_scalar_kernel(
+            'PS0_train_atomic.npy', 'PS0_train_atomic_sparse.npy', zeta=zeta,
+            kernel_name='K0_NM', workdir=workdir)
     # test-train(sparse)
-    compute_kernel('PS0_test_atomic.npy', 'PS0_train_atomic_sparse.npy',
-                   zeta=zeta, kernel_name='K0_TM', workdir=workdir)
+    compute_scalar_kernel('PS0_test_atomic.npy', 'PS0_train_atomic_sparse.npy',
+                          zeta=zeta, kernel_name='K0_TM', workdir=workdir)
 
+
+def compute_scalar_residual(n_max, l_max, atom_width, rad_r0, rad_m,
+                            dipole_reg, charge_reg, n_sparse_envs=2000,
+                            n_sparse_components=500, workdir=None,
+                            dipole_normalize=True, recompute_kernels=True):
+    if recompute_kernels:
+        recompute_scalar_kernels(n_max, l_max, atom_width, rad_r0, rad_m,
+                                 n_sparse_envs, n_sparse_components, workdir)
     # compute weights (velociraptor)
     dipoles_train = np.load(os.path.join(workdir, 'dipoles_train.npy'))
     geoms_train = ase.io.read(os.path.join(workdir, 'qm7_train.xyz'), ':')
@@ -194,5 +208,3 @@ if __name__ == "__main__":
     print("Success? {}".format(result.success))
     print(result.message)
     print(result.x)
-    #dipole_rmse = compute_scalar_residual(8, 6, 0.5, 3, 2, 1.0, 100.0,
-    #                                      "/scratch/veit/dipoles/cv_0")
